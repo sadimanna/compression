8e2ddaec5057467a70e26b5471e623e99de0ee0a
Siladittya Manna <sadimanna@users.noreply.github.com>
2018-03-03 19:55:27 +0530

    Add files via upload

---
+++ lzw.m
@@ -0,0 +1,71 @@
+%% Lempel-Ziv-Welch
+%img = imread('Tagore_Gandhi.jpg');
+%img = imresize(img,0.25);
+%gimg = rgb2gray(img);
+%gimgt = gimg';
+%flattened_img = gimgt(:);
+
+lenimg = size(flattened_img);
+lenimg = lenimg(1)
+
+compressed_img = [];
+dict = 1;
+%% Create a Container 
+code = containers.Map('KeyType','char','ValueType','uint64');
+
+%% Assigning the first 256 gray values to first 256 codes
+for i=0:255
+    code(num2str(i)) = i;
+end
+next_code = 256;
+%% Creating the encoded dictionary
+tic
+string = '';
+prev_code = '';
+for i=1:lenimg
+    if mod(i,1000)==0
+        disp('Compressing...')
+    end
+    input = num2str(flattened_img(i));
+    if isequal('',string)
+        string = input;
+    else
+        prev_string = string;
+        string = [string,' ',input]; %#ok<*AGROW>
+    end
+    
+    if ismember(string,keys(code))
+        %disp('In Dictionary, Continue...')
+        dict = 1;
+        continue
+    else
+        dict = 0;
+        %disp('Not in Dictionary...Add to Dictionary...')
+        code(string) = next_code;
+        next_code = next_code + 1;
+        string = input;
+    end
+    %valueset = values(code);
+    prev_code = code(prev_string);
+    compressed_img = [compressed_img,prev_code];
+end
+disp('Adding the Last Code')
+prev_code = code(string);
+compressed_img = [compressed_img,prev_code];
+toc
+%% Evaluation
+maxcodeval = next_code;
+maxbits = length(de2bi(maxcodeval));
+comp_img_bytes = length(compressed_img)*maxbits/8;
+disp('Compressed Image')
+disp(compressed_img)
+disp('Highest Value of Code in compressed image')
+disp(maxcodeval)
+disp('Number of bits required for compression')
+disp(maxbits)
+disp('Number of bytes in Original Image')
+disp(lenimg)
+disp('Number of Bytes in Compressed Image')
+disp(comp_img_bytes)
+disp('Compression Ratio')
+disp(comp_img_bytes/lenimg)
